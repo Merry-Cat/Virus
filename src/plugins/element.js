69ba5ca6bd598b361fc39dd762d0e8260cd0b537
仲夏十三 <thxmei@vip.qq.com>
2020-03-14 14:37:30 +0800

    修复vant样式出错问题

--- src/plugins/element.js
+++
@@ -1,4 +0,0 @@
-/* import Vue from 'vue';
-import {Message,} from 'element-ui';
-//Vue.use()
-Vue.prototype.$message = Message */
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports={
 	configureWebpack: (config)=>{
 	if(process.env.NODE_ENV === 'production'){
 		config.externals = {
-			'vue':'Vue',
 			'echarts':'echarts',
 			'vue-aswsome-swiper':'VueAwesomeSwiper',
 			'axios':'axios',
